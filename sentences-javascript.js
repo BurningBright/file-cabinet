7a93190f7dcf179afb93ffceaeba5da4e9b45773
BurningBright <lcg51271@gmail.com>
2016-04-20 17:53:53 +0800

    /* add maven command, split windows & js command */

---
+++ sentences-javascript.js
@@ -0,0 +1,74 @@
+(function(arg){...})(param);
+(function($) {...})(jquery);
+
+var a=function($) {...};
+a(jquery);
+
+jQuery(document).ready(function(){ });
+
+//时间插件-中文
+$("#TSMGRAM_REALCA_1_birthday").datepicker({
+	inline:false,
+	dateFormat: "yy-mm-dd",
+	dayNames: [ "星期天", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"],
+	dayNamesMin: ["日", "一", "二", "三", "四", "五", "六"],
+	monthNamesShort: [ "1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月" ]
+});
+
+//验证
+if( this.value=="" || ( this.value!="" && !/.+@.+\.[a-zA-Z]{2,4}$/.test(this.value) ) ){
+
+
+$("#TSMGRAM_REALCA_1_realSub").click(function(event){			//提交时做判断
+	event.preventDefault();										//阻止提交
+	var flag = true;
+																//对必要字段做检查
+	$("#TSMGRAM_REALCA_1_form :input.required").each(function(){
+		if( $(this).is('#TSMGRAM_REALCA_1_username') ){
+			if( this.value.length < 1 ){
+				$("#TSMGRAM_REALCA_1_msgWapper").text("无效用户名");
+				return flag = false;
+			}
+		}
+		if( $(this).is('#TSMGRAM_REALCA_1_fullname') ){
+			if( this.value.length < 1 ){
+				$("#TSMGRAM_REALCA_1_msgWapper").text("无效用户全称");
+				return flag = false;
+			}
+		}
+		if( $(this).is('#TSMGRAM_REALCA_1_sex') ){
+			if( this.value.length < 1 ){
+				$("#TSMGRAM_REALCA_1_msgWapper").text("无效性别");
+				return flag = false;
+			}
+		}
+		if( $(this).is('#TSMGRAM_REALCA_1_birthday') ){
+			if( this.value=="" ){
+				$("#TSMGRAM_REALCA_1_msgWapper").text("无效生日");
+				return flag = false;
+			}
+		}
+		if( $(this).is('#TSMGRAM_REALCA_1_id') ){
+			if( this.value.length < 11 ){
+				$("#TSMGRAM_REALCA_1_msgWapper").text("无效身份证");
+				return flag = false;
+			}
+		}
+		if( $(this).is('#TSMGRAM_REALCA_1_email') ){
+			if( this.value=="" || ( this.value!="" && !/.+@.+\.[a-zA-Z]{2,4}$/.test(this.value) )){
+				$("#TSMGRAM_REALCA_1_msgWapper").text("无效邮箱");
+				return flag = false;
+			}
+		}
+		if( $(this).is('#TSMGRAM_REALCA_1_verify') ){
+			if( this.value.length != 4 ){
+				$("#TSMGRAM_REALCA_1_msgWapper").text("无效验证码");
+				return flag = false;
+			}
+		}
+	});
+	
+	if( flag ){
+		$("#TSMGRAM_REALCA_1_form").submit();
+	}
+});
