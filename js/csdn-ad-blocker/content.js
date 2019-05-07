6a1fb5ff99f829c8ea6a81fe2806653f9fbae9fe
BurningBright <lcg51271@gmail.com>
2019-05-07 22:39:39 +0800

    /** more ad, show more */

--- js/csdn-ad-blocker/content.js
+++ js/csdn-ad-blocker/content.js
@@ -2,10 +2,18 @@ let adSelectors = [
   'iframe',
   'aside .csdn-tracking-statistics.mb8.box-shadow',
   'aside .right_box.footer_box.csdn-tracking-statistics',
-  '.recommend-right'
+  '.indexSuperise',
+  '#adContent',
+  '#reportContent',
+  '.recommend-box',
+  '.recommend-right',
+  '.hide-article-box.hide-article-pos.text-center'
 ]
 
 document.querySelectorAll(adSelectors.join(',')).forEach(item => {
   item.style.display = 'none'
 })
 
+var readmore = document.getElementById("btn-readmore");
+readmore.removeAttribute('data-track-click');
+readmore.click();
